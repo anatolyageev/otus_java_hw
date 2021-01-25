6e2bcf009adc12662d978a99e19d37764a0578a3
Anatolii Ageev <anatol1812@gmail.com>
2021-01-25 21:26:54 +0200

    added result info

--- hw03-annotation/src/main/java/com/otus/ageev/mytestframework/MyTestFramework.java
+++ hw03-annotation/src/main/java/com/otus/ageev/mytestframework/MyTestFramework.java
@@ -16,31 +16,35 @@ public class MyTestFramework {
     private int successCount;
     private int failureCount;
 
-public void testAll(String className) throws ClassNotFoundException {
-    Class<?> testClass = Class.forName(className);
-    getAnnotatedMethods(testClass);
+    public void testAll(String className) throws ClassNotFoundException {
+        Class<?> testClass = Class.forName(className);
+        getAnnotatedMethods(testClass);
 
-    for(Method testMethod : testMethods){
-        try {
-            Object obj = testClass.getDeclaredConstructor().newInstance();
-
-            invokeMethods(obj,beforeMethods);
-            testMethod.invoke(obj);
-            invokeMethods(obj,afterMethods);
-            successCount++;
-        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-            e.printStackTrace();
-            failureCount++;
+        for (Method testMethod : testMethods) {
+            try {
+                Object obj = testClass.getDeclaredConstructor().newInstance();
+                invokeMethods(obj, beforeMethods);
+                testMethod.invoke(obj);
+                invokeMethods(obj, afterMethods);
+                successCount++;
+            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                e.printStackTrace();
+                failureCount++;
+            } finally {
+                testCount++;
+            }
         }
-        finally {
-            testCount++;
-        }
+        printResult();
     }
 
-}
+    private void printResult() {
+        System.out.println("Test count: " + testCount);
+        System.out.println("Success test count: " + successCount);
+        System.out.println("Failure test count: " + failureCount);
+    }
 
     private void invokeMethods(Object obj, List<Method> methods) throws IllegalAccessException, InvocationTargetException {
-        for (Method method : methods){
+        for (Method method : methods) {
             method.invoke(obj);
         }
     }
