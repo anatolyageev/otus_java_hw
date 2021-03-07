8ae42fbe33260927c64bdac2b4667931b9169524
Anatolii Agieiev <anatol1812@gmail.com>
2021-03-07 14:10:21 +0200

    Complited HW06

--- hw06-oop/src/main/java/otus/java/ageeev/App.java
+++
@@ -1,4 +0,0 @@
-package otus.java.ageeev;
-
-public class App {
-}
--- hw06-oop/src/main/java/otus/java/ageeev/service/AtmCassetteService.java
+++ hw06-oop/src/main/java/otus/java/ageeev/service/AtmCassetteService.java
@@ -2,6 +2,7 @@ package otus.java.ageeev.service;
 
 import otus.java.ageeev.domain.Denomination;
 import otus.java.ageeev.exeption.NotEnoughNotesInRepository;
+import otus.java.ageeev.exeption.OutOfMaxCapacityInCassette;
 import otus.java.ageeev.repository.AtmCassetteRepository;
 
 public class AtmCassetteService {
@@ -11,20 +12,33 @@ public class AtmCassetteService {
         this.cassetteRepository = cassetteRepository;
     }
 
-    public Long getCashDenomination(Denomination denomination, int notesNumber) {
+    private Long getCashDenomination(Denomination denomination, int notesNumber) {
         return cassetteRepository.getSumFromCassette(denomination, notesNumber);
     }
 
     public Long getSum(Long sum) {
         Long sumWithdraw = Long.valueOf(0);
         if (sum <= cassetteRepository.getTotalMoneyAmount()) {
-            for(var entry : cassetteRepository.getFromRepo(sum).entrySet()){
-                sumWithdraw+= getCashDenomination(entry.getKey(),entry.getValue());
+            for (var entry : cassetteRepository.getFromRepo(sum).entrySet()) {
+                sumWithdraw += getCashDenomination(entry.getKey(), entry.getValue());
             }
         } else {
-            throw  new NotEnoughNotesInRepository("Not enought notes");
+            throw new NotEnoughNotesInRepository("Not enought notes");
         }
         return sumWithdraw;
+    }
+
+    public boolean depositCash(Denomination denomination, int notesNum) {
+        try {
+            cassetteRepository.putCash(denomination, notesNum);
+        } catch (OutOfMaxCapacityInCassette ex) {
+            return false;
+        }
+        return true;
+    }
+
+    public Long totalMoneyAmount() {
+        return cassetteRepository.getTotalMoneyAmount();
     }
 
 }
